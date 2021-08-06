64ffe553a3691c470dec2b32797fb7d5ff260b23
emi johnson <emijobin27@outlook.com>
2021-08-06 12:16:20 +0100

    Add project files.

---
+++ Dice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create object for rollingdice/ constructor is called here
+
+            RollingDice objsixside = new RollingDice(10);
+            RollingDice objtenside = new RollingDice();
+
+            Console.WriteLine(objsixside.SideCount());
+            Console.WriteLine(objtenside.SideCount());
+
+           // Console.WriteLine(objsixside);
+            
+
+            // roll the 6 side dice
+
+            Console.WriteLine(objsixside);
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine(objsixside.RollCount());
+            }
+            // roll the 10 side die
+            Console.WriteLine(objtenside);
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(objtenside.RollCount());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dice/RollingDice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class RollingDice
+    {
+        // fields for sides and Random number
+        private int sides;
+        private Random random;
+        // create a constructor for class to intialize sides and randomfields
+        public RollingDice()
+        {
+            sides = 6;
+            random = new Random();
+        }
+        public RollingDice(int totalsides)
+        {
+            sides = totalsides;
+            random = new Random();
+        }
+        // create a method/function for no:of sides or sidecount
+
+        public int SideCount()
+        {
+            return sides;
+        }
+        public int RollCount()
+        {
+            return random.Next (1,sides+1);
+        }
+        
+        public override string ToString()
+        {
+            return string.Format(" Rolling a die with{0} sides",sides);
+        }
+
+    }
+}
